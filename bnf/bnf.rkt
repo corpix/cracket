e66585f79061f339f2fdafcd4e0998b6c54c3400
Dmitry Moskowski <me@corpix.ru>
2023-01-26 03:57:57 +0000

    bnf: provide symbols prefixed with module name

--- bnf/bnf.rkt
+++ bnf/bnf.rkt
@@ -1,99 +1,96 @@
 #lang racket
 
-(provide parser-error
-         parser-error?
-         parser-error-state
-         parser-error-buf
-         parser-error-rule
-         buffer-not-exhausted-error
-         buffer-not-exhausted-error?
-         buffer-not-exhausted-error-state
-         buffer-not-exhausted-error-buf
-         (struct-out parser-node)
-         (struct-out parser-state)
+(provide (except-out (struct-out bnf-exn)
+                     -make-bnf-exn)
+         (except-out (struct-out bnf-parser-not-exhausted-exn)
+                     -make-bnf-parser-not-exhausted-exn)
+         (struct-out bnf-node)
+         (except-out (struct-out bnf-state)
+                     -make-bnf-state)
+         make-bnf-state
          define-bnf)
 
-(define-struct (parser-error exn)
+(define-struct (bnf-exn exn)
   (state buf rule)
-  #:constructor-name -make-parser-error)
+  #:constructor-name -make-bnf-exn)
 
-(define make-parser-error*
+(define make-bnf-exn*
   (case-lambda
     ((state buf rule)
-     (make-parser-error*
+     (make-bnf-exn*
       state buf rule
       (format "Failed to parse ~s with rule ~a at ~a."
 	      (if buf (bytes->string/utf-8 buf) "")
 	      'body
-	      (parser-state-pos state))
+	      (bnf-state-pos state))
       (current-continuation-marks)))
     ((state buf rule message cont)
-     (-make-parser-error message cont state buf rule))))
+     (-make-bnf-exn message cont state buf rule))))
 
-(define-struct (buffer-not-exhausted-error exn)
+(define-struct (bnf-parser-not-exhausted-exn exn)
   (state buf)
-  #:constructor-name -make-buffer-not-exhausted-error)
+  #:constructor-name -make-bnf-parser-not-exhausted-exn)
 
-(define make-buffer-not-exhausted-error*
+(define make-bnf-parser-not-exhausted-exn*
   (case-lambda
     ((state buf)
-     (make-buffer-not-exhausted-error*
+     (make-bnf-parser-not-exhausted-exn*
       state buf
       (format "input buffer is not exhausted, last position: ~a, data left in buffer: ~s"
-	      (parser-state-pos state)
+	      (bnf-state-pos state)
 	      (if buf (bytes->string/utf-8 buf) ""))
       (current-continuation-marks)))
     ((state buf message cont)
-     (-make-buffer-not-exhausted-error message cont state buf))))
+     (-make-bnf-parser-not-exhausted-exn message cont state buf))))
 
 ;;
 
-(define-struct parser-node
+(define-struct bnf-node
   (name rule match child)
   #:transparent)
 
 ;;
 
-(define-struct parser-state (buf pos depth)
-  #:constructor-name -make-parser-state
+(define-struct bnf-state (buf pos depth)
+  #:constructor-name -make-bnf-state
   #:mutable)
 
-(define (make-parser-state buf)
-  (-make-parser-state
+(define (make-bnf-state buf)
+  (-make-bnf-state
    (cond ((string? buf) (string->bytes/utf-8 buf))
          (else buf))
    0 0))
 
-(define (parser-state-load! state state-copy)
-  (set-parser-state-buf! state (parser-state-buf state-copy))
-  (set-parser-state-pos! state (parser-state-pos state-copy))
-  (set-parser-state-depth! state (parser-state-depth state-copy)))
+(define (bnf-state-load! state state-copy)
+  (set-bnf-state-buf! state (bnf-state-buf state-copy))
+  (set-bnf-state-pos! state (bnf-state-pos state-copy))
+  (set-bnf-state-depth! state (bnf-state-depth state-copy)))
 
-(define (parser-state-match! state name rule do-match)
-  (let ((buf (parser-state-buf state))
+(define (bnf-state-match! state name rule do-match)
+  (let ((buf (bnf-state-buf state))
         (match (do-match state)))
     (and match
-         (parser-node
+         (bnf-node
           name rule
           (subbytes buf 0
                     (- (bytes-length buf)
-                       (bytes-length (parser-state-buf state))))
+                       (bytes-length (bnf-state-buf state))))
           match))))
 
-(define (parser-state-commit! state n)
-  (begin0 (subbytes (parser-state-buf state) 0 n)
-    (set-parser-state-buf!
+(define (bnf-state-commit! state n)
+  (begin0 (subbytes (bnf-state-buf state) 0 n)
+    (set-bnf-state-buf!
      state
-     (subbytes (parser-state-buf state)
-               n (bytes-length (parser-state-buf state))))
-    (set-parser-state-pos!
+     (subbytes (bnf-state-buf state)
+               n (bytes-length (bnf-state-buf state))))
+    (set-bnf-state-pos!
      state
-     (+ (parser-state-pos state) n))))
+     (+ (bnf-state-pos state) n))))
 
 ;; ~bnf-rule-aux expands a rule and returns a lambda
 ;; which receives `state` and returns (vector ...) or #f.
 ;; it is ok for vector to be empty.
-;; results should not be wrapped into `parser-node`.
+;; results should not be wrapped into `bnf-node`.
 (define-syntax (~bnf-rule-aux stx)
   (syntax-case stx (or and * + lambda)
     ((_ (or rule ...))
@@ -105,17 +102,17 @@
               (do-match (lambda (state)
                           (let loop ((acc (vector)) (ms matchers))
                             (if (pair? ms)
-                                (let ((s (bytes->string/utf-8 (parser-state-buf state)))
+                                (let ((s (bytes->string/utf-8 (bnf-state-buf state)))
                                       (match ((car ms) state)))
                                   (and match (loop
                                               (vector-append acc (vector match))
                                               (cdr ms))))
                                 acc)))))
          (lambda (state)
-           (let* ((state-copy (struct-copy parser-state state))
+           (let* ((state-copy (struct-copy bnf-state state))
                   (match (do-match state-copy)))
              (begin0 match
-               (and match (parser-state-load! state state-copy))))))))
+               (and match (bnf-state-load! state state-copy))))))))
     ((_ (* rule))
      (syntax/loc stx
        (let ((do-match (~bnf-rule-aux rule)))
@@ -145,12 +142,12 @@
                ((string? rule-val)
                 (syntax/loc stx (let ((r (string->bytes/utf-8 rule)))
                                   (lambda (state)
-                                    (and (>= (bytes-length (parser-state-buf state))
+                                    (and (>= (bytes-length (bnf-state-buf state))
                                              (bytes-length r))
-                                         (let ((sub (subbytes (parser-state-buf state)
+                                         (let ((sub (subbytes (bnf-state-buf state)
                                                                  0 (bytes-length r))))
                                            (and (equal? sub r)
-                                                (parser-state-commit! state (bytes-length r))
+                                                (bnf-state-commit! state (bytes-length r))
                                                 (vector))))))))
                ((symbol? rule-val) (syntax/loc stx rule))
                (else (error "Expected symbol or string, got" rule-val)))))
@@ -158,7 +155,7 @@
 
 ;; ~bnf-parse-aux expands all rules providen and
 ;; returns lambda which consumes an input to provide
-;; the `parser-node` or raise an exception in case:
+;; the `bnf-node` or raise an exception in case:
 ;; - there was no match in `input`
 ;; - input buffer is not drained (not all data was read)
 (define-syntax (~bnf-parse-aux stx)
@@ -166,34 +163,39 @@
     ((_ ((var val) ...) body)
      (syntax/loc stx
        (let* ((var  (lambda (state)
-                      (parser-state-match!
-                       state 'var 'val
-                       (~bnf-rule-aux val))))
+                      (bnf-state-match! state 'var 'val
+                                        (~bnf-rule-aux val))))
               ...
-              (proc (lambda (state) (parser-state-match!
-                                     state #f 'body
-                                     (~bnf-rule-aux body)))))
+              (proc (lambda (state) (bnf-state-match! state #f 'body
+                                                      (~bnf-rule-aux body)))))
          (lambda (input)
-           (let* ((state (make-parser-state input))
+           (let* ((state (make-bnf-state input))
                   (child (proc state))
-                  (buf   (and child (parser-state-buf state))))
+                  (buf   (and child (bnf-state-buf state))))
              ;; TODO: this is an extension point, we probably want to have
              ;; an implementation which would not fail with exceptions
              ;; of such kind (non exhausting)
              (begin0 child
                (unless child
-                 (raise (make-parser-error* state buf 'body)))
+                 (raise (make-bnf-exn* state buf 'body)))
                (when (> (bytes-length buf) 0)
-                 (raise (make-buffer-not-exhausted-error* state buf)))))))))))
+                 (raise (make-bnf-parser-not-exhausted-exn* state buf)))))))))))
 
-;; define-bnf defines a parser with `name`
+;; let-bnf is like let, but for bnf rules.
+(define-syntax (let-bnf stx)
+  (syntax-case stx ()
+    ((_ rules body)
+     (syntax/loc stx (~bnf-parse-aux rules body)))))
+
+;; define-bnf defines a bnf with `name`
 ;; which is represented by a lambda
 ;; using `rules` and `body` for parsing
 ;; input passed to a lambda as first argument.
 (define-syntax (define-bnf stx)
   (syntax-case stx ()
     ((_ name rules body)
-     (syntax/loc stx (define name (~bnf-parse-aux rules body))))))
+     (syntax/loc stx (define name (let-bnf rules body))))))
+
 
 
 (module+ test
@@ -207,36 +209,36 @@
 
     (check-equal?
      (terminals-test "test")
-     (parser-node
+     (bnf-node
       #f 't/y (list->bytes '(116 101 115 116))
-      (parser-node
+      (bnf-node
        't/y '(or t y) (list->bytes '(116 101 115 116))
-       (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))
+       (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))
     (check-equal?
      (terminals-test (list->bytes '(116 101 115 116)))
-     (parser-node
+     (bnf-node
       #f 't/y (list->bytes '(116 101 115 116))
-      (parser-node
+      (bnf-node
        't/y '(or t y) (list->bytes '(116 101 115 116))
-       (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))
+       (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))
     (check-equal?
      (terminals-test "you")
-     (parser-node
+     (bnf-node
       #f 't/y (list->bytes '(121 111 117))
-      (parser-node
+      (bnf-node
        't/y '(or t y) (list->bytes '(121 111 117))
-       (parser-node 'y "you" (list->bytes '(121 111 117)) (vector)))))
+       (bnf-node 'y "you" (list->bytes '(121 111 117)) (vector)))))
     (check-equal?
      (terminals-test (list->bytes '(121 111 117)))
-     (parser-node
+     (bnf-node
       #f 't/y (list->bytes '(121 111 117))
-      (parser-node
+      (bnf-node
        't/y '(or t y) (list->bytes '(121 111 117))
-       (parser-node 'y "you" (list->bytes '(121 111 117)) (vector)))))
+       (bnf-node 'y "you" (list->bytes '(121 111 117)) (vector)))))
 
-    (check-exn buffer-not-exhausted-error? (thunk (terminals-test "testyou")))
-    (check-exn parser-error? (thunk (terminals-test "y")))
-    (check-exn parser-error? (thunk (terminals-test ""))))
+    (check-exn bnf-parser-not-exhausted-exn? (thunk (terminals-test "testyou")))
+    (check-exn bnf-exn? (thunk (terminals-test "y")))
+    (check-exn bnf-exn? (thunk (terminals-test ""))))
   (test-case "booleans"
     (define-bnf booleans-test
       ((t "test")
@@ -246,52 +248,52 @@
       ty&ty)
     (check-equal?
      (booleans-test "testyou")
-     (parser-node
+     (bnf-node
       #f 'ty&ty (list->bytes '(116 101 115 116 121 111 117))
-      (parser-node
+      (bnf-node
        'ty&ty '(and ty ty) (list->bytes '(116 101 115 116 121 111 117))
        (vector
-        (parser-node 'ty '(or t y) (list->bytes '(116 101 115 116))
-                     (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))
-        (parser-node 'ty '(or t y) (list->bytes '(121 111 117))
-                     (parser-node 'y "you" (list->bytes '(121 111 117)) (vector)))))))
+        (bnf-node 'ty '(or t y) (list->bytes '(116 101 115 116))
+                     (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))
+        (bnf-node 'ty '(or t y) (list->bytes '(121 111 117))
+                     (bnf-node 'y "you" (list->bytes '(121 111 117)) (vector)))))))
     (check-equal?
      (booleans-test "youtest")
-     (parser-node
+     (bnf-node
       #f 'ty&ty (list->bytes '(121 111 117 116 101 115 116))
-      (parser-node
+      (bnf-node
        'ty&ty '(and ty ty) (list->bytes '(121 111 117 116 101 115 116))
        (vector
-        (parser-node 'ty '(or t y) (list->bytes '(121 111 117))
-                     (parser-node 'y "you" (list->bytes '(121 111 117)) (vector)))
-        (parser-node 'ty '(or t y) (list->bytes '(116 101 115 116))
-                     (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))))
+        (bnf-node 'ty '(or t y) (list->bytes '(121 111 117))
+                     (bnf-node 'y "you" (list->bytes '(121 111 117)) (vector)))
+        (bnf-node 'ty '(or t y) (list->bytes '(116 101 115 116))
+                     (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))))
 
     (check-equal?
      (booleans-test "testtest")
-     (parser-node
+     (bnf-node
       #f 'ty&ty (list->bytes '(116 101 115 116 116 101 115 116))
-      (parser-node
+      (bnf-node
        'ty&ty '(and ty ty) (list->bytes '(116 101 115 116 116 101 115 116))
        (vector
-        (parser-node 'ty '(or t y) (list->bytes '(116 101 115 116))
-                     (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))
-        (parser-node 'ty '(or t y) (list->bytes '(116 101 115 116))
-                     (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))))
+        (bnf-node 'ty '(or t y) (list->bytes '(116 101 115 116))
+                     (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))
+        (bnf-node 'ty '(or t y) (list->bytes '(116 101 115 116))
+                     (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))))
     (check-equal?
      (booleans-test "youyou")
-     (parser-node
+     (bnf-node
       #f 'ty&ty (list->bytes '(121 111 117 121 111 117))
-      (parser-node
+      (bnf-node
        'ty&ty '(and ty ty) (list->bytes '(121 111 117 121 111 117))
        (vector
-        (parser-node 'ty '(or t y) (list->bytes '(121 111 117))
-                     (parser-node 'y "you" (list->bytes '(121 111 117)) (vector)))
-        (parser-node 'ty '(or t y) (list->bytes '(121 111 117))
-                     (parser-node 'y "you" (list->bytes '(121 111 117)) (vector)))))))
-    (check-exn buffer-not-exhausted-error? (thunk (booleans-test "testyoutest")))
-    (check-exn parser-error? (thunk (booleans-test "y")))
-    (check-exn parser-error? (thunk (booleans-test ""))))
+        (bnf-node 'ty '(or t y) (list->bytes '(121 111 117))
+                     (bnf-node 'y "you" (list->bytes '(121 111 117)) (vector)))
+        (bnf-node 'ty '(or t y) (list->bytes '(121 111 117))
+                     (bnf-node 'y "you" (list->bytes '(121 111 117)) (vector)))))))
+    (check-exn bnf-parser-not-exhausted-exn? (thunk (booleans-test "testyoutest")))
+    (check-exn bnf-exn? (thunk (booleans-test "y")))
+    (check-exn bnf-exn? (thunk (booleans-test ""))))
   (test-case "repeat zero or more"
     (define-bnf repeat*-test
       ((t "test")
@@ -300,31 +302,31 @@
 
     (check-equal?
      (repeat*-test "")
-     (parser-node #f 't* #"" (parser-node 't* '(* t) #"" (vector))))
+     (bnf-node #f 't* #"" (bnf-node 't* '(* t) #"" (vector))))
     (check-equal?
      (repeat*-test "test")
-     (parser-node
+     (bnf-node
       #f 't* (list->bytes '(116 101 115 116))
-      (parser-node
+      (bnf-node
        't* '(* t) (list->bytes '(116 101 115 116))
-       (vector (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
+       (vector (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
     (check-equal?
      (repeat*-test "testtest")
-     (parser-node
+     (bnf-node
       #f 't* (list->bytes '(116 101 115 116 116 101 115 116))
-      (parser-node
+      (bnf-node
        't* '(* t) (list->bytes '(116 101 115 116 116 101 115 116))
-       (vector (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))
-               (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
+       (vector (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))
+               (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
     (check-equal?
      (repeat*-test "testtesttest")
-     (parser-node
+     (bnf-node
       #f 't* (list->bytes '(116 101 115 116 116 101 115 116 116 101 115 116))
-      (parser-node
+      (bnf-node
        't* '(* t) (list->bytes '(116 101 115 116 116 101 115 116 116 101 115 116))
-       (vector (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))
-               (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))
-               (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))))
+       (vector (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))
+               (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))
+               (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector)))))))
   (test-case "repeat one and more"
     (define-bnf repeat+-test
       ((t "test")
@@ -333,25 +335,25 @@
 
     (check-equal?
      (repeat+-test "test")
-     (parser-node
+     (bnf-node
       #f 't+ (list->bytes '(116 101 115 116))
-      (parser-node
+      (bnf-node
        't+ '(+ t) (list->bytes '(116 101 115 116))
-       (vector (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
+       (vector (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
     (check-equal?
      (repeat+-test "testtest")
-     (parser-node
+     (bnf-node
       #f 't+ (list->bytes '(116 101 115 116 116 101 115 116))
-      (parser-node
+      (bnf-node
        't+ '(+ t) (list->bytes '(116 101 115 116 116 101 115 116))
-       (vector (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))
-               (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
+       (vector (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))
+               (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))
     (check-equal?
      (repeat+-test "testtesttest")
-     (parser-node
+     (bnf-node
       #f 't+ (list->bytes '(116 101 115 116 116 101 115 116 116 101 115 116))
-      (parser-node
+      (bnf-node
        't+ '(+ t) (list->bytes '(116 101 115 116 116 101 115 116 116 101 115 116))
-       (vector (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))
-               (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))
-               (parser-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))))
+       (vector (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))
+               (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))
+               (bnf-node 't "test" (list->bytes '(116 101 115 116)) (vector))))))))
