436ec5491296c4287e5b63260f8c1a4a0118d925
Dmitry Moskowski <me@corpix.ru>
2023-02-01 00:58:53 +0000

    firewalld: update nix derivation

--- firewalld/default.nix
+++ firewalld/default.nix
@@ -1,16 +1,29 @@
 { pkgs ? import <nixpkgs> {}, ... }:
-with pkgs;
 let
+  inherit (pkgs)
+    fetchFromGitHub
+    stdenv
+    makeWrapper
+    cracket
+    libcap
+    iptables
+  ;
+  inherit (pkgs.lib)
+    makeBinPath
+  ;
+
   overlay = import ../overlay.nix { inherit pkgs; };
 in stdenv.mkDerivation {
   name = "firewalld";
   src = ./.;
   buildInputs = [overlay.cracket];
+  nativeBuildInputs = [makeWrapper];
   buildPhase = ''
     raco exe -o firewalld firewalld.rkt
   '';
   installPhase = ''
     mkdir -p $out/bin
     mv firewalld $out/bin
+    wrapProgram $out/bin/firewalld --prefix PATH ":" ${makeBinPath [libcap iptables]} 
   '';
 }
