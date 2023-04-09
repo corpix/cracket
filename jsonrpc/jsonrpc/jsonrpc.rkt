8d5b65279c762b7008bbd393ce32e8cbe7c9558d
Dmitry Moskowski <me@corpix.ru>
2023-04-09 16:10:44 +0000

    jsonrpc: tcp server & client

--- jsonrpc/jsonrpc/jsonrpc.rkt
+++ jsonrpc/jsonrpc/jsonrpc.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require racket/async-channel
+         racket/tcp
          corpix/json
          (for-syntax corpix/syntax))
 (module+ test
@@ -22,6 +23,12 @@
   #:constructor-name -make-jsonrpc-request-registry
   #:transparent)
 
+(define-struct jsonrpc-client (in out closer)
+  #:transparent)
+
+(define (close-jsonrpc-client client)
+  ((jsonrpc-client-closer client)))
+
 (define (make-jsonrpc-request-registry)
   (-make-jsonrpc-request-registry (make-hash)
                                   (make-semaphore 1)))
@@ -29,12 +36,19 @@
 ;;
 
 (define jsonrpc-version "2.0")
-(define current-jsonrpc-port
+(define current-jsonrpc-input-port
   (make-derived-parameter (make-parameter #f)
                           values
                           (lambda (port)
                             (begin0 port
-                              (unless port (error "no jsonrpc port is active"))))))
+                              (unless port (error "no input jsonrpc port is active"))))))
+
+(define current-jsonrpc-output-port
+  (make-derived-parameter (make-parameter #f)
+                          values
+                          (lambda (port)
+                            (begin0 port
+                              (unless port (error "no output jsonrpc port is active"))))))
 
 (define current-jsonrpc-id-generator (make-parameter (make-jsonrpc-id-counter)))
 
@@ -43,10 +57,11 @@
 
 ;;
 
-(define (write-jsonrpc-message id payload (port (current-jsonrpc-port)))
-  (write-json (make-hasheq `((jsonrpc . ,jsonrpc-version) (id . ,id) ,@payload)) port))
+(define (write-jsonrpc-message id payload (port (current-jsonrpc-output-port)))
+  (write-json (make-hasheq `((jsonrpc . ,jsonrpc-version) (id . ,id) ,@payload)) port)
+  (flush-output port))
 
-(define (write-jsonrpc-request method (port (current-jsonrpc-port))
+(define (write-jsonrpc-request method (port (current-jsonrpc-output-port))
                                #:params (params #f)
                                #:next-id (next-id (current-jsonrpc-id-generator)))
   (let ((id (next-id)))
@@ -55,15 +70,15 @@
              (payload (if params (cons `(params . ,params) payload) payload)))
         (write-jsonrpc-message id payload port)))))
 
-(define (write-jsonrpc-response id (port (current-jsonrpc-port))
+(define (write-jsonrpc-response id (port (current-jsonrpc-output-port))
                                 #:result (result #f)
                                 #:error (error #f))
   (let* ((payload (if error
                       `((error . ,error))
-                      `((result . ,result)))))
+                      `((result . ,(if (void? result) 'null result))))))
     (write-jsonrpc-message id payload port)))
 
-(define (read-jsonrpc-message (port (current-jsonrpc-port)))
+(define (read-jsonrpc-message (port (current-jsonrpc-input-port)))
   (let* ((message (read-json port))
          (message-version (hash-ref message "jsonrpc" #f)))
     (begin0 message
@@ -72,13 +87,13 @@
       (unless (equal? message-version jsonrpc-version)
         (error (format "jsonrpc version mismatch: expected ~s, got ~s" jsonrpc-version message-version))))))
 
-(define (read-jsonrpc-request (port (current-jsonrpc-port)))
+(define (read-jsonrpc-request (port (current-jsonrpc-input-port)))
   (let* ((message (read-jsonrpc-message port)))
     (begin0 message
       (unless (hash-has-key? message "method")
         (error (format "jsonrpc method is not specified: ~a" message))))))
 
-(define (read-jsonrpc-response (port (current-jsonrpc-port)))
+(define (read-jsonrpc-response (port (current-jsonrpc-input-port)))
   (let* ((message (read-jsonrpc-message port)))
     (begin0 message
       (unless (or (hash-has-key? message "result")
@@ -170,16 +185,18 @@
 (define (dispatch-jsonrpc-response message
                                    #:request-registry (registry (current-jsonrpc-request-registry)))
   (let* ((id (hash-ref message "id"))
-         (chan (hash-ref registry id #f)))
+         (chan (call-with-semaphore
+                (jsonrpc-request-registry-sem registry)
+                (thunk (hash-ref (jsonrpc-request-registry-channels registry) id #f)))))
     (unless chan
       (error (format "jsonrpc response dispatch failed, no response channel found for id ~a" id)))
     (async-channel-put
      chan
      (or (hash-ref message "result" #f)
-         (make-exn:fail (hash-ref message "result")
+         (make-exn:fail (hash-ref message "error")
                         (current-continuation-marks))))))
 
-(define (invoke-jsonrpc method (port (current-jsonrpc-port))
+(define (invoke-jsonrpc method (port (current-jsonrpc-output-port))
                         #:params (params null)
                         #:request-registry (registry (current-jsonrpc-request-registry)))
   (let ((id (write-jsonrpc-request method port #:params params))
@@ -211,7 +228,7 @@
                                            ("method" . "foo")
                                            ("params" . #hash(("bar" . 1) ("baz" . 2))))))))
   (test-case "dispatch-jsonrpc-request"
-    (parameterize ((current-jsonrpc-port (open-output-string))
+    (parameterize ((current-jsonrpc-output-port (open-output-string))
                    (current-jsonrpc-id-generator (make-jsonrpc-id-counter 777))
                    (current-jsonrpc-request-registry (make-jsonrpc-request-registry))
                    (current-jsonrpc-method-registry (make-hash)))
@@ -219,30 +236,77 @@
         "This will wrap bar & baz into list and return."
         (list bar baz))
       (invoke-jsonrpc 'foo #:params '(1 2))
-      (let ((in (open-input-string (get-output-string (current-jsonrpc-port)))))
-        (check-equal? (dispatch-jsonrpc-request (read-jsonrpc-request in))
-                      '(1 2)))))
+      (parameterize ((current-jsonrpc-input-port (open-input-string (get-output-string (current-jsonrpc-output-port)))))
+        (check-equal? (dispatch-jsonrpc-request (read-jsonrpc-request)) '(1 2))))))
 
-  )
+(define (dispatch-jsonrpc-requests (in (current-jsonrpc-input-port))
+                                (out (current-jsonrpc-output-port)))
+  (let loop ()
+    (let* ((request (read-jsonrpc-request in))
+           (id (hash-ref request "id"))
+           ;; TODO: bounded worker pool to dispatch requests concurrently
+           (result (with-handlers ((exn? identity)) (dispatch-jsonrpc-request request))))
+      (if (exn? result)
+          (write-jsonrpc-response id out #:error (exn-message result)) ;; TODO: reconstruct continuation marks?
+          (write-jsonrpc-response id out #:result result)))
+    (loop)))
 
-;; ;;
+(define (dispatch-jsonrpc-responses (in (current-jsonrpc-input-port)))
+  (let loop ()
+    (dispatch-jsonrpc-response (read-jsonrpc-response in))
+    (loop)))
+
+(define (call-jsonrpc method
+                      (in (current-jsonrpc-input-port))
+                      (out (current-jsonrpc-output-port))
+                      #:params (params null))
+  (let* ((result (async-channel-get (invoke-jsonrpc method #:params params)))
+         (result (if (eq? result (current-json-null)) (void) result)))
+    (begin0 result
+      (when (exn? result)
+        (raise result)))))
+
+(define (jsonrpc-tcp-serve host port
+                           #:dispatcher (dispatcher dispatch-jsonrpc-requests)
+                           #:backlog (backlog 128)
+                           #:reuse? (reuse? #t))
+  (let* ((listener (tcp-listen port backlog reuse? host))
+         (conn-dispatcher (thunk (let-values (((in out) (tcp-accept listener)))
+                                   (dispatcher in out))))
+         (conn-dispatcher-thread (thread conn-dispatcher)))
+    (thunk (kill-thread conn-dispatcher-thread)
+           (tcp-close listener))))
+
+(define (jsonrpc-tcp-client host port
+                          #:dispatcher (dispatcher dispatch-jsonrpc-responses))
+  (let*-values (((in out) (tcp-connect host port))
+                ((conn-dispatcher-thread) (thread (thunk (dispatcher in)))))
+    (make-jsonrpc-client in out (thunk (kill-thread conn-dispatcher-thread)))))
+
+(define-syntax (with-jsonrpc stx)
+  (syntax-parse stx
+    ((_ client-expr body ...)
+     #'(let ((client client-expr))
+         (parameterize ((current-jsonrpc-input-port (jsonrpc-client-in client))
+                        (current-jsonrpc-output-port (jsonrpc-client-out client)))
+           body ...)))))
+
+;;
 
 ;; (define-jsonrpc (foo bar baz)
-;;   "Returns foo, using positional arguments"
-;;   (list 'args=> bar baz))
+;;   (displayln (list 'foo-called bar baz))
+;;   (list bar baz))
 
-;; (define-jsonrpc (foo-kw #:bar bar #:baz baz)
-;;   "Returns foo, using kw arguments as input"
-;;   (list 'args=> bar baz))
+;; (define-jsonrpc (foo-void bar baz)
+;;   (displayln (list 'foo-void-called bar baz)))
 
-;; (current-jsonrpc-method-registry)
+;; (define-jsonrpc (bar #:baz baz #:qux (qux 'hello))
+;;   (displayln (list 'bar-called baz qux))
+;;   (make-hash `((baz . ,baz) (qux . ,qux))))
 
-;; (dispatch-jsonrpc-request
-;;  (make-hash '(("method" . "foo")
-;;               ("params" . (1 2)))))
-;; (dispatch-jsonrpc-request
-;;  (make-hash '(("method" . "foo-kw")
-;;               ("params" . #hash(("bar" . 1) (baz . 2))))))
-
-;; {"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}
-;; {"jsonrpc": "2.0", "method": "subtract", "params": {"subtrahend": 23, "minuend": 42}, "id": 3}
+;; (define stop (jsonrpc-tcp-serve "127.0.0.1" 9094))
+;; (define client (jsonrpc-tcp-client "127.0.0.1" 9094))
+;; (with-jsonrpc client
+;;   (call-jsonrpc 'foo #:params '(1 2))
+;;   (call-jsonrpc 'foo-void #:params '(1 2))
+;;   (call-jsonrpc 'bar #:params (make-hash '((baz . 1)))))
