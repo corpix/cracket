61ae085135d92be0b5a03630f9e0c31e14f8e06b
Dmitry Moskowski <me@corpix.ru>
2023-07-16 10:38:21 +0000

    websocket: fix dependency list

--- websocket/info.rkt
+++ websocket/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define pkg-name "corpix-websocket")
 (define collection "corpix")
-(define deps '("base"))
+(define deps '("base" "corpix-http" "corpix-url"))
 (define build-deps '("rackunit-lib"))
 (define version "0.0")
 (define pkg-authors '(corpix))
