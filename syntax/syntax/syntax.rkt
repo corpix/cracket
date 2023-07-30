58e4ef2f3f2c08fb129654d4f19ab5a93e125d7c
Dmitry Moskowski <me@corpix.ru>
2023-07-30 16:10:25 +0000

    syntax: reprovide syntax/parse/experimental/template

--- syntax/syntax/syntax.rkt
+++ syntax/syntax/syntax.rkt
@@ -2,11 +2,13 @@
 (require racket/syntax
          racket/syntax-srcloc
          syntax/strip-context
-         syntax/parse)
+         syntax/parse
+         syntax/parse/experimental/template)
 (provide (all-from-out racket/syntax
                        racket/syntax-srcloc
                        syntax/strip-context
-                       syntax/parse)
+                       syntax/parse
+                       syntax/parse/experimental/template)
          syntax-srcdir)
 
 (define (syntax-srcdir stx)
