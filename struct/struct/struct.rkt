4d30f67f1452ba2492741eaa8895254212b091a0
Dmitry Moskowski <me@corpix.ru>
2023-08-03 00:09:14 +0000

    struct: add struct-data proc

--- struct/struct/struct.rkt
+++ struct/struct/struct.rkt
@@ -1,7 +1,10 @@
 #lang racket
 (require corpix/list)
-(provide struct-id)
+(provide (all-defined-out))
 
 (define (struct-id struct)
   (let ((type (car (values->list (struct-info struct)))))
     (car (values->list (struct-type-info type)))))
+
+(define (struct-data struct)
+  (cdr (vector->list (struct->vector struct))))
