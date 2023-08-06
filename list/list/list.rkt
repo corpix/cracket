0a2e8766014e2ff18bb1b626913403b6650aaf2b
Dmitry Moskowski <me@corpix.ru>
2023-08-06 22:03:13 +0000

    list: ass enumerate procedure

--- list/list/list.rkt
+++ list/list/list.rkt
@@ -1,6 +1,12 @@
 #lang racket
-(provide values->list)
+(provide values->list
+         enumerate)
 
 (define-syntax (values->list stx)
   (syntax-case stx ()
     ((_ expr) (syntax (call-with-values (lambda () expr) list)))))
+
+(define (enumerate lst)
+  (for/list ((value (in-list lst))
+             (index (in-naturals)))
+    (list value index)))
